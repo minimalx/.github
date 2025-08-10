b4b57a9bf1700c8e6c80e138f426c5a55509452d
jakub <jakub@minimalx.com>
2025-08-10 12:48:25 +0100

    sh edit

--- actions/build-stm32/entrypoint.sh
+++ actions/build-stm32/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euo pipefail
+set -eu
 
 stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -importAll "$1"
 headless-build.sh -data /tmp/stm-workspace -build "$2"
