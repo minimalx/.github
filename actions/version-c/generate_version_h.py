eeb6bb00205f3b21225350de532c1a7f078ddbf2
jakub <jakub@minimalx.com>
2025-08-27 15:32:17 +0100

    add proejct root for version

--- actions/version-c/generate_version_h.py
+++ actions/version-c/generate_version_h.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 import argparse
-from datetime import datetime
+from datetime import datetime, timezone
 from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--version', required=True, help='Semantic version string')
     parser.add_argument('--actor', required=True, help='GitHub actor')
+    parser.add_argument('--root', required=False, default=".", help='folder containing the project')
     args = parser.parse_args()
 
     version = args.version
     major, minor, patch = version.split('.', 2)
     patch = patch.split('-', 1)[0]
-    date = datetime.utcnow().strftime('%Y-%m-%d')
-
+    date = datetime.now(timezone.utc).strftime('%Y-%m-%d')
     content = f"""#ifndef VERSION_H
 #define VERSION_H
 
@@ -28,8 +28,12 @@ static const char VERSION_AUTHOR[]   = "{args.actor}";
 #endif // VERSION_H
 """
 
-    # Check for Core/Inc directory
-    target_dir = Path("Core/Inc") if Path("Core/Inc").is_dir() else Path(".")
+    # Check for {}/Core/Inc directory
+    if Path(f"{args.root}/Core/Inc").is_dir():
+        target_dir = Path(f"{args.root}/Core/Inc")
+    else:
+        raise FileNotFoundError(f"{args.root}/Core/Inc directory not found")
+    
     target_file = target_dir / "version.h"
 
     with open(target_file, 'w') as f:
